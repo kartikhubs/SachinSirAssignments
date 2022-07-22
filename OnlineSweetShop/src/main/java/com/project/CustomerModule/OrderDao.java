4f40b2dd3f0a3778c26611b2f4a6ebb89944bbc3
kartikhubs <52950858+kartikhubs@users.noreply.github.com>
2022-07-22 21:12:44 +0530

    Delete OnlineSweetShop directory

--- OnlineSweetShop/src/main/java/com/project/CustomerModule/OrderDao.java
+++
@@ -1,7 +0,0 @@
-package com.project.CustomerModule;
-
-public interface OrderDao {
-	public void placeorder(OrderPojo object); // place an order
-
-	public void vieworder(int orderid, String sweetname, String brand); // view the order
-}
